7bc38fcd098fb38dd9980818cd2297e9cf6a4e80
ufcyg <mellowski@gmail.com>
2021-03-09 10:32:34 +0100

    added exception handling for folder creation

--- src/ScheduledTask/FileCreationTaskHandler.php
+++ src/ScheduledTask/FileCreationTaskHandler.php
@@ -2,15 +2,20 @@
 
 namespace ASScheduledTaskTest\ScheduledTask;
 
+use ASMailService\Core\MailServiceHelper;
+use Exception;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
 use Shopware\Core\Framework\MessageQueue\ScheduledTask\ScheduledTaskHandler;
 
 
 class FileCreationTaskHandler extends ScheduledTaskHandler
 {
-    
-    public function __construct(EntityRepositoryInterface $scheduledTaskRepository)
+    /** @var MailServiceHelper $mailserviceHelper */
+    private $mailserviceHelper;
+    public function __construct(EntityRepositoryInterface $scheduledTaskRepository,
+                                MailServiceHelper $mailserviceHelper)
     {
+        $this->mailserviceHelper = $mailserviceHelper;
         parent::__construct($scheduledTaskRepository);
     }
 
@@ -21,9 +26,15 @@ class FileCreationTaskHandler extends ScheduledTaskHandler
 
     public function run(): void
     {
-        $folderName = bin2hex(random_bytes(5));
-        if (!file_exists('../custom/plugins/ASScheduledTaskTest/' . $folderName)) {
-            mkdir('../custom/plugins/ASScheduledTaskTest/' . $folderName, 0777, true);
+        try {
+            $folderName = bin2hex(random_bytes(5));
+            if (!file_exists('../custom/plugins/ASScheduledTaskTest/' . $folderName)) {
+                mkdir('../custom/plugins/ASScheduledTaskTest/' . $folderName, 0777, true);
+            }
+        }
+        catch (Exception $e){
+            $notificationSalesChannel = $this->systemConfigService->get('SynlabOrderInterface.config.fallbackSaleschannelNotification');
+            $this->asMailService->sendMyMail(['patrick.thimm@synlab.com'=>'patrick thimm'],$notificationSalesChannel,'cronjobtester','this is a testmail',$e,$e,['']);
         }
     }    
 }
